73e8461cfc44c0261bce0bd0f41d62aba93bf841
Aldrich Agabin <arcflu52@gmail.com>
2022-03-28 16:06:05 -0400

    Testing Pages (non-functional)
    Added testing pages

---
+++ pages/Dashboard.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Button } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+
+const Dashboard = () => (
+    <Text>
+        Bruh
+    </Text>
+)
+
+export default Dashboard
---
+++ pages/Friends.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Button } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+function Friends(){
+    return (
+        <Text>
+            Friends
+        </Text>
+    )
+}
+
+export default Friends
