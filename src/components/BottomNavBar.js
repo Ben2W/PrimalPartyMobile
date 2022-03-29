45714a732fc9bed45646290d74504f4707eb7fb9
Aldrich Agabin <arcflu52@gmail.com>
2022-03-28 21:04:52 -0400

    Refactored to src directory

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
 import Dashboard from "../pages/Dashboard";
 
-const DashboardRoute = () => <Text>Dashboard</Text>;
+const DashboardRoute = () => Dashboard;
 
 const InvitesRoute = () => <Text>Invites</Text>;
 
@@ -21,7 +21,7 @@ const BottomNavBar = () => {
     ]);
 
     const renderScene = BottomNavigation.SceneMap({
-        dashboard: DashboardRoute,
+        dashboard: Dashboard,
         invites: InvitesRoute,
         friends: FriendsRoute,
         myevents: MyEventsRoute,
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,11 +3,12 @@ import { Button } from 'react-native-paper';
 import { StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
 
-
 const Dashboard = () => (
-    <Text>
-        Bruh
-    </Text>
+    <View>
+        <Text>
+            Bruh
+        </Text>
+    </View>
 )
 
 export default Dashboard
