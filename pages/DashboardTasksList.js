0770b6cc7210d827c88aacf13411ab8331facc0d
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-04-09 15:59:44 -0400

    add fetch to task page

--- pages/DashboardTasksList.js
+++ pages/DashboardTasksList.js
@@ -1,9 +1,40 @@
-import {List, Text, View} from "native-base";
+import React, { useState, useEffect } from 'react'
+import { List, Text, View } from "native-base";
 import ListItem from "native-base/src/components/primitives/List/ListItem";
 
 
-const DashboardTasksList = () =>{
-    return(
+const DashboardTasksList = () => {
+
+    const [tasks, setTasks] = useState([])
+
+    const fetchTasks = async () => {
+        const url = 'http://localhost:8080/tasks'
+
+        try {
+            const res = await fetch(url,
+                {
+                    method: 'GET',
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+                    },
+                    credentials: 'include'
+                })
+            const tasks = await res.json()
+            console.log(tasks)
+            setTasks(tasks)
+            // return events.events
+        } catch (e) {
+            return e
+        }
+    }
+
+    useEffect(() => {
+        fetchTasks()
+    }, [tasks])
+
+
+
+    return (
         <View>
             <List>
                 <ListItem>
