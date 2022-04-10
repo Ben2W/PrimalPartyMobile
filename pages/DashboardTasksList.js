35f45fb6136542eddcc6f25514cc725b135ca548
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-04-10 14:48:25 -0400

    build the task page without context

--- pages/DashboardTasksList.js
+++ pages/DashboardTasksList.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { List, Text, View } from "native-base";
-import ListItem from "native-base/src/components/primitives/List/ListItem";
+import React, { useState, useEffect, useLayoutEffect, useContext } from 'react'
+import { List, Text, View, Spinner, Center, Box, Heading } from "native-base";
+import { CredentialsContext } from "../components/CredentialsContext";
+import { ScrollView } from "react-native";
+import CustomTaskCard from '../components/CustomTaskCard'
 
 
-const DashboardTasksList = () => {
+const DashboardTasksList = ({ navigation }) => {
 
     const [tasks, setTasks] = useState([])
+    const [taskArray, setTaskArray] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [firstName, setFirstName] = useState(useContext(CredentialsContext).storedCredentials.firstName)
 
     const fetchTasks = async () => {
         const url = 'http://localhost:8080/tasks'
@@ -20,30 +25,63 @@ const DashboardTasksList = () => {
                     credentials: 'include'
                 })
             const tasks = await res.json()
-            console.log(tasks)
-            setTasks(tasks)
-            // return events.events
+            setTasks(tasks.tasks)
+            return tasks.tasks
         } catch (e) {
             return e
         }
     }
 
+    const setTaskList = async (currTasks) => {
+        let tempTasks = []
+        for (let task of currTasks) {
+            tempTasks.push(<CustomTaskCard navigation={navigation} key={task.id} data={task}></CustomTaskCard>);
+        }
+        setTaskArray(tempTasks);
+    }
+
     useEffect(() => {
         fetchTasks()
-    }, [tasks])
+            .then((currTasks) => {
+                setTaskList(currTasks)
+                    .then(() => {
+                        setLoading(false);
+                    }
+                    )
+            }).catch(e => {
+                return e
+            })
 
-
+    }, [])
 
     return (
-        <View>
-            <List>
-                <ListItem>
-                    <Text>
-                        Task 1
-                    </Text>
-                </ListItem>
-            </List>
-        </View>
+        loading ?
+            <Center h="100%">
+                < Box >
+                    <Spinner size="lg" />
+                    <Heading color="#397367" fontSize="md">
+                        Welcome to {firstName}'s tasks!
+                    </Heading>
+                </Box >
+            </Center >
+            :
+            (
+                <View style={{
+                    flex: 1,
+                    top: "8%",
+                    marginLeft: "2%",
+                    marginRight: "2%",
+                }}>
+                    <Box maxH={"90%"} flexGrow={1}>
+                        <Heading textAlign={"center"} style={{ paddingBottom: '5%' }}>
+                            Your tasks
+                        </Heading>
+                        <ScrollView>
+                            {taskArray}
+                        </ScrollView>
+                    </Box>
+                </View>
+            )
     );
 }
 
